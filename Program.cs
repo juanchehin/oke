e17ed6f5d830530a0a3eb80b611a0f6b527451be
chehin <chehinjuan@gmail.com>
2021-11-16 20:54:58 -0300

    Primeros pasos en capaa presentacion

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace oke
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Presentacion.Usuarios());
         }
     }
 }
