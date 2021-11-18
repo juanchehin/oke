4e4e560966a9107dfc56b8d002d012e0faba72cf
chehin <chehinjuan@gmail.com>
2021-11-18 08:10:26 -0300

    Metodos de abrir y cerrar conexion

--- Datos/ConexionMaestra.cs
+++ Datos/ConexionMaestra.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using System.Data;
 
 namespace oke.Datos
 {
     internal static class ConexionMaestra
     {
         public static SqlConnection conexion = new SqlConnection(@"Data Source=DESKTOP-9BKC47G\SQLEXPRESS; Initial Catalog=oke; Integrated Security=true");
+
+        public static void abrir()
+        {
+            if(conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+        public static void cerrar()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
     }
 }
