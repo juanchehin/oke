00322025859e685bb1cfb24c6c2374bc1eda60f6
chehin <chehinjuan@gmail.com>
2021-11-21 09:47:46 -0300

    Demas botones y logo en menu principal

--- oke-desarrollo/oke/Presentacion/MenuPrincipal.Designer.cs
+++ oke-desarrollo/oke/Presentacion/MenuPrincipal.Designer.cs
@@ -31,75 +31,38 @@ namespace oke.Presentacion
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPedidos = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.lblLogin = new System.Windows.Forms.Label();
-            this.btnPedidos = new System.Windows.Forms.Button();
+            this.btnCatalogo = new System.Windows.Forms.Button();
+            this.btnPedir = new System.Windows.Forms.Button();
+            this.btnGenerarQR = new System.Windows.Forms.Button();
+            this.btnMesas = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.panel4.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.btnMesas);
+            this.panel1.Controls.Add(this.btnGenerarQR);
+            this.panel1.Controls.Add(this.btnPedir);
+            this.panel1.Controls.Add(this.btnCatalogo);
             this.panel1.Controls.Add(this.btnPedidos);
             this.panel1.Controls.Add(this.panel4);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(189, 506);
+            this.panel1.Size = new System.Drawing.Size(221, 639);
             this.panel1.TabIndex = 0;
-            // 
-            // panel2
-            // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panel2.Location = new System.Drawing.Point(189, 0);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(92, 506);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(281, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(605, 506);
-            this.panel3.TabIndex = 2;
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
-            this.panel4.Controls.Add(this.lblLogin);
-            this.panel4.Controls.Add(this.pictureBox1);
-            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel4.Location = new System.Drawing.Point(0, 0);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(189, 100);
-            this.panel4.TabIndex = 0;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(57, 50);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // lblLogin
-            // 
-            this.lblLogin.Dock = System.Windows.Forms.DockStyle.Right;
-            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLogin.ForeColor = System.Drawing.Color.Tomato;
-            this.lblLogin.Location = new System.Drawing.Point(66, 0);
-            this.lblLogin.Name = "lblLogin";
-            this.lblLogin.Size = new System.Drawing.Size(123, 100);
-            this.lblLogin.TabIndex = 1;
-            this.lblLogin.Text = "Usuario";
-            this.lblLogin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // btnPedidos
             // 
@@ -115,13 +78,140 @@ namespace oke.Presentacion
             this.btnPedidos.Size = new System.Drawing.Size(180, 64);
             this.btnPedidos.TabIndex = 1;
             this.btnPedidos.Text = "Pedidos";
+            this.btnPedidos.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnPedidos.UseVisualStyleBackColor = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.panel4.Controls.Add(this.lblLogin);
+            this.panel4.Controls.Add(this.pictureBox1);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(221, 100);
+            this.panel4.TabIndex = 0;
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.ForeColor = System.Drawing.Color.Tomato;
+            this.lblLogin.Location = new System.Drawing.Point(98, 0);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(123, 100);
+            this.lblLogin.TabIndex = 1;
+            this.lblLogin.Text = "Usuario";
+            this.lblLogin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(221, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(92, 639);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(313, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(573, 639);
+            this.panel3.TabIndex = 2;
+            // 
+            // btnCatalogo
+            // 
+            this.btnCatalogo.BackColor = System.Drawing.Color.Transparent;
+            this.btnCatalogo.FlatAppearance.BorderSize = 0;
+            this.btnCatalogo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnCatalogo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnCatalogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCatalogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCatalogo.ForeColor = System.Drawing.Color.White;
+            this.btnCatalogo.Location = new System.Drawing.Point(0, 173);
+            this.btnCatalogo.Name = "btnCatalogo";
+            this.btnCatalogo.Size = new System.Drawing.Size(180, 64);
+            this.btnCatalogo.TabIndex = 2;
+            this.btnCatalogo.Text = "Catalogo";
+            this.btnCatalogo.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnCatalogo.UseVisualStyleBackColor = false;
+            // 
+            // btnPedir
+            // 
+            this.btnPedir.BackColor = System.Drawing.Color.Transparent;
+            this.btnPedir.FlatAppearance.BorderSize = 0;
+            this.btnPedir.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnPedir.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnPedir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPedir.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPedir.ForeColor = System.Drawing.Color.White;
+            this.btnPedir.Location = new System.Drawing.Point(6, 229);
+            this.btnPedir.Name = "btnPedir";
+            this.btnPedir.Size = new System.Drawing.Size(180, 64);
+            this.btnPedir.TabIndex = 3;
+            this.btnPedir.Text = "Pedir";
+            this.btnPedir.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnPedir.UseVisualStyleBackColor = false;
+            // 
+            // btnGenerarQR
+            // 
+            this.btnGenerarQR.BackColor = System.Drawing.Color.Transparent;
+            this.btnGenerarQR.FlatAppearance.BorderSize = 0;
+            this.btnGenerarQR.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnGenerarQR.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnGenerarQR.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerarQR.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerarQR.ForeColor = System.Drawing.Color.White;
+            this.btnGenerarQR.Location = new System.Drawing.Point(3, 299);
+            this.btnGenerarQR.Name = "btnGenerarQR";
+            this.btnGenerarQR.Size = new System.Drawing.Size(180, 64);
+            this.btnGenerarQR.TabIndex = 4;
+            this.btnGenerarQR.Text = "Generar QR";
+            this.btnGenerarQR.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGenerarQR.UseVisualStyleBackColor = false;
+            // 
+            // btnMesas
+            // 
+            this.btnMesas.BackColor = System.Drawing.Color.Transparent;
+            this.btnMesas.FlatAppearance.BorderSize = 0;
+            this.btnMesas.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnMesas.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnMesas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMesas.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMesas.ForeColor = System.Drawing.Color.White;
+            this.btnMesas.Location = new System.Drawing.Point(6, 369);
+            this.btnMesas.Name = "btnMesas";
+            this.btnMesas.Size = new System.Drawing.Size(180, 64);
+            this.btnMesas.TabIndex = 5;
+            this.btnMesas.Text = "Mesas";
+            this.btnMesas.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnMesas.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.HotPink;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(3, 450);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(212, 186);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
             // 
             // MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(886, 506);
+            this.ClientSize = new System.Drawing.Size(886, 639);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -132,6 +222,7 @@ namespace oke.Presentacion
             this.panel1.ResumeLayout(false);
             this.panel4.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -145,5 +236,10 @@ namespace oke.Presentacion
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnMesas;
+        private System.Windows.Forms.Button btnGenerarQR;
+        private System.Windows.Forms.Button btnPedir;
+        private System.Windows.Forms.Button btnCatalogo;
     }
 }
