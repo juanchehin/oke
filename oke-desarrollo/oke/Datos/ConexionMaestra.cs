488936c87b733913ce723b5081cc5245d0fd0427
chehin <chehinjuan@gmail.com>
2021-11-21 10:51:58 -0300

    Conexion maestra

---
+++ oke-desarrollo/oke/Datos/ConexionMaestra.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace oke.Datos
+{
+    public class ConexionMaestra
+    {
+        public static string conexion = @"Data source=DESKTOP-9BKC47G\SQLEXPRESS;Initial Catalog=oke;Integrated Security=true";
+        public static SqlConnection conectar = new SqlConnection(conexion);
+        public static void abrir()
+        {
+            if(conectar.State == ConnectionState.Closed)
+            {
+                conectar.Open();
+            }
+        }
+
+        public static void cerrar()
+        {
+            if (conectar.State == ConnectionState.Open)
+            {
+                conectar.Close();
+            }
+        }
+    }
+}
