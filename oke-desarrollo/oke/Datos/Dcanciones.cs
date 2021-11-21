e859e1a9582785371702f939fe930b6bdbbd1d44
chehin <chehinjuan@gmail.com>
2021-11-21 11:01:20 -0300

    Clase Dcanciones

---
+++ oke-desarrollo/oke/Datos/Dcanciones.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using oke.Logica;
+using System.Windows.Forms;
+using oke.Datos;
+
+namespace KaraokeCurso.Datos
+{
+	public class Dcanciones
+	{
+		public bool insertar_Canciones(Lcanciones parametros)
+		{
+			try
+			{
+				ConexionMaestra.abrir();
+				SqlCommand cmd = new SqlCommand("insertar_canciones", ConexionMaestra.conectar);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@Nombre", parametros.Nombre);
+				cmd.ExecuteNonQuery();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.StackTrace);
+				return false;
+			}
+			finally
+			{
+				ConexionMaestra.cerrar();
+			}
+		}
+		public bool eliminar_Canciones(Lcanciones parametros)
+		{
+			try
+			{
+				ConexionMaestra.abrir();
+				SqlCommand cmd = new SqlCommand("eliminar_canciones", ConexionMaestra.conectar);
+				cmd.CommandType = CommandType.StoredProcedure;
+				cmd.Parameters.AddWithValue("@IdCancion", parametros.IdCancion);
+				cmd.ExecuteNonQuery();
+				return true;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.StackTrace);
+				return false;
+			}
+			finally
+			{
+				ConexionMaestra.cerrar();
+			}
+		}
+		public void buscarCanciones(ref DataTable dt, string buscador)
+		{
+			try
+			{
+				ConexionMaestra.abrir();
+				SqlDataAdapter da = new SqlDataAdapter("buscar_canciones", ConexionMaestra.conectar);
+				da.SelectCommand.CommandType = CommandType.StoredProcedure;
+				da.SelectCommand.Parameters.AddWithValue("@letra", buscador);
+				da.Fill(dt);
+			}
+			catch (Exception ex)
+			{
+
+				MessageBox.Show(ex.StackTrace);
+
+			}
+			finally
+			{
+				ConexionMaestra.cerrar();
+			}
+		}
+	}
+}
