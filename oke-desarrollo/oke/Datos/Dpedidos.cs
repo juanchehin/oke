bfb912f8bfc23036df7b1397b75a5341f1cb6f2e
chehin <chehinjuan@gmail.com>
2021-11-25 11:56:53 -0300

    inicio con pedidos + BD

---
+++ oke-desarrollo/oke/Datos/Dpedidos.cs
@@ -0,0 +1,100 @@
+﻿using oke.Logica;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oke.Datos
+{
+    public class Dpedidos
+    {
+        public bool Insertar_Pedidos(Lpedidos parametros)
+        {
+            try
+            {
+                ConexionMaestra.abrir();
+                SqlCommand cmd = new SqlCommand("insertar_Pedidos", ConexionMaestra.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@IdMesa", parametros.IdMesa);
+                cmd.Parameters.AddWithValue("@IdCancion", parametros.IdCancion);
+                cmd.Parameters.AddWithValue("@Mensaje", parametros.Mensaje);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                ConexionMaestra.cerrar();
+            }
+        }
+
+        public bool EliminarPedidos(Lpedidos parametros)
+        {
+            try
+            {
+                ConexionMaestra.abrir();
+                SqlCommand cmd = new SqlCommand("eliminar_pedidos", ConexionMaestra.conectar);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Idpedido", parametros.IdPedido);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                ConexionMaestra.cerrar();
+            }
+        }
+
+        public bool eliminarPedidosTodos()
+        {
+            try
+            {
+                ConexionMaestra.abrir();
+                SqlCommand cmd = new SqlCommand("eliminar_todos_pedidos", ConexionMaestra.conectar);
+                cmd.ExecuteNonQuery();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            finally
+            {
+                ConexionMaestra.cerrar();
+            }
+        }
+
+        public void MostrarPedidos(ref DataTable dt)
+        {
+            try
+            {
+                ConexionMaestra.abrir();
+                SqlDataAdapter da = new SqlDataAdapter("mostrarPedidos", ConexionMaestra.conectar);
+                da.Fill(dt);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace);
+            }
+            finally
+            {
+                ConexionMaestra.cerrar();
+            }
+        }
+    }
+}
---
+++ oke-desarrollo/oke/Logica/Lpedidos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oke.Logica
+{
+    public class Lpedidos
+    {
+        public int IdPedido { get; set; }
+        public int IdMesa { get; set; }
+        public int IdCancion { get; set; }
+        public string Estado { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
