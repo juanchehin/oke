7c4b5cb32e8a016096a0acc2053d40c09a9f2414
chehin <chehinjuan@gmail.com>
2022-02-17 14:26:47 -0300

    gradiente en feliz cumple

--- oke-desarrollo/okeApp/okeApp/okeApp/Vistas/FelizCumple.xaml.cs
+++ oke-desarrollo/okeApp/okeApp/okeApp/Vistas/FelizCumple.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using Xamarin.Forms;
@@ -15,6 +16,23 @@ namespace okeApp.Vistas
         public FelizCumple()
         {
             InitializeComponent();
+            Task.Run(AnimacionDegradado);
         }
+
+        int IdMesa = 0;
+        private CancellationTokenSource TimerCancelar;
+        private async void AnimacionDegradado()
+        {
+            Action<double> frontal = input => bvGradiente.AnchorY = input;
+            Action<double> atras = input => bvGradiente.AnchorY = input;
+            while (true)
+            {
+                bvGradiente.Animate(name: "frontal", callback: frontal, start: 0, end: 1, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+                bvGradiente.Animate(name: "atras", callback: atras, start: 1, end: 0, length: 5000, easing: Easing.SinIn);
+                await Task.Delay(5000);
+            }
+        }
+
     }
 }
