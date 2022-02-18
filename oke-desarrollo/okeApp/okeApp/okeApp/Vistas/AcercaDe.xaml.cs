5444d8e6972e11daefa36a94c307ab1570575aa7
chehin <chehinjuan@gmail.com>
2022-02-18 15:51:39 -0300

    Pantalla de acerca de

---
+++ oke-desarrollo/okeApp/okeApp/okeApp/Vistas/AcercaDe.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Rg.Plugins.Popup.Services;
+using System;
+using Xamarin.Forms.Xaml;
+
+namespace okeApp.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AcercaDe : PopupPage
+    {
+        public AcercaDe()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnOk_Clicked(object sender, EventArgs e)
+        {
+            await PopupNavigation.Instance.PopAllAsync();
+        }
+    }
+}
--- oke-desarrollo/okeApp/okeApp/okeApp/Vistas/Pedidos.xaml.cs
+++ oke-desarrollo/okeApp/okeApp/okeApp/Vistas/Pedidos.xaml.cs
@@ -131,15 +131,15 @@ namespace okeApp.Vistas
 
         }
 
-        private async void btnFelizcumple_Clicked(object sender, EventArgs e)
+        private void btnFelizcumple_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new FelizCumple());
+   
         }
 
-        private void btnAcercade_Clicked(object sender, EventArgs e)
+        private async void btnAcercade_Clicked(object sender, EventArgs e)
         {
-            // await PopupNavigation.Instance.PushAsync(new AcercaDe());
-            DisplayAlert("Hola","Acerca de","Ok");
+            await PopupNavigation.Instance.PushAsync(new AcercaDe());
         }
     }
 }
