d679949921c85d889022ecff8e1cac98cd7ba1d2
chehin <chehinjuan@gmail.com>
2022-02-18 16:42:12 -0300

    Iconos del apk

--- oke-desarrollo/okeApp/okeApp/okeApp.Android/MainActivity.cs
+++ oke-desarrollo/okeApp/okeApp/okeApp.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using ZXing.Mobile;
 
 namespace okeApp.Droid
 {
-    [Activity(Label = "okeApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "okeApp", Icon = "@mipmap/oke", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
