6a41a8529b0efcd244eb590663c9532552691c75
chehin <chehinjuan@gmail.com>
2022-02-21 16:54:06 -0300

    Color letra

--- oke-desarrollo/oke/Presentacion/GenerarQR.Designer.cs
+++ oke-desarrollo/oke/Presentacion/GenerarQR.Designer.cs
@@ -39,6 +39,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(271, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(174, 20);
@@ -58,6 +59,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
             this.label2.Location = new System.Drawing.Point(22, 88);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(280, 13);
@@ -68,6 +70,7 @@
             // 
             this.txtIp.AutoSize = true;
             this.txtIp.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIp.ForeColor = System.Drawing.Color.White;
             this.txtIp.Location = new System.Drawing.Point(308, 84);
             this.txtIp.Name = "txtIp";
             this.txtIp.Size = new System.Drawing.Size(122, 18);
